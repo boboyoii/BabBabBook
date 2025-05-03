945b090f05e0daca4fea839a3a1ec891bc150e32
boboyoii <139124147+boboyoii@users.noreply.github.com>
2025-05-03 12:26:15 +0900

    feat: 로고 버튼 누르면 홈화면으로 이동

---
+++ js/common.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const logo = document.querySelector('.logo');
+  logo.addEventListener('click', function () {
+    window.location.href = 'home.html';
+  });
+});
