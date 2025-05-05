6100b3664ba62f61856a6ed2c04280fba9ef98ba
boboyoii <139124147+boboyoii@users.noreply.github.com>
2025-05-05 16:42:55 +0900

    feat: 뒤로가기 버튼 홈화면 -> 이전 페이지 수정

--- js/common.js
+++ js/common.js
@@ -3,4 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
   logo.addEventListener('click', function () {
     window.location.href = 'home.html';
   });
+
+  const backBtn = document.querySelector('.back-btn');
+
+  if (backBtn) {
+    backBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      window.history.back();
+    });
+  }
 });
